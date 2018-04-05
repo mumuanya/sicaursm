bc356febdb413d7a17dc07c4c23edf938ca9bc78
mrruan <314445437@qq.com>
2018-04-05 09:56:30 +0800

    测试dao类

--- src/com/rk/test/TestDao.java
+++ src/com/rk/test/TestDao.java
@@ -3,7 +3,6 @@ package com.rk.test;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
